a6bf52edbee93bae54d55477c80cbbc2e8a01eb4
GigioR <gioprib@gmail.com>
2020-06-05 18:24:05 -0300

    more two screens ready and part of back-end

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -28,12 +28,15 @@ function getCities (event) {
     stateInput.value = event.target.options[indexOfSelectedState].text
     
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
+    
+    
+    citySelect.innerHTML = "<option value=''>Selecione a Cidade</option>"
+    citySelect.disabled = true
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for( city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false;
@@ -43,3 +46,46 @@ function getCities (event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)  
+
+// Itens de coleta
+// Pegar todos os <li>
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(let item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+let collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLI = event.target;
+    //Toggle: add or remove a class 
+    itemLI.classList.toggle("selected");
+
+    const itemId = itemLI.dataset.id;
+
+    // verify if there are items selected
+
+    // if the item is selected, add to  the selection
+
+    // if the item is already on the list and it's selected again, remove it from the collection
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        return item === itemId
+    })
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            return item != itemId
+        })
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems
+}
